0cf53706404f6abc4bbc376f60adad8cd8c7a51e
Ischi94 <gmathes94@gmx.de>
2021-08-18 10:43:26 +0200

    update code with varying atmospheric dC13

--- pCO2_code.R
+++ pCO2_code.R
@@ -1,7 +1,7 @@
 # load libraries
 library(tidyverse) # for data processing
 library(openxlsx) # for writing to excel file
-
+library(boot) # bootstrapping
 
 # functions 
 # set up mean function for bootstrapping
@@ -14,6 +14,20 @@ median.fun <- function(pCO2_long, pCO2) median(pCO2_long[pCO2], na.rm = TRUE)
 # load data
 sol_dat <- readxl::read_excel("paleosol_data.xlsx")
 
+# clean column names
+sol_dat <- sol_dat %>% 
+  # remove empty columns
+  select(-c(5, 10)) %>% 
+  # rename columns
+  rename("d13C_atm_min" = "d13C atm", 
+         "d13C_atm_max" = "...4", 
+         "soil_temp_min" = "T",
+         "soil_temp_max" = "...8", 
+         "S_z_min" = "S(z)", 
+         "S_z_max" = "...12") %>% 
+  # remove empty rows
+  drop_na(Sample)
+  
 # set-up ------------------------------------------------------------------
 
 # set up data frame for output
@@ -49,27 +63,44 @@ for(i in temp_range){
 # preallocate data frame
 pCO2 <- tibble(Sz = seq(500, 1500, 100), 
                temp_range = temp_range, empty_row = rep(0,length(temp_range))) %>% 
-  pivot_wider(names_from = temp_range, values_from = empty_row, names_prefix = "temp_")
+  pivot_wider(names_from = temp_range, 
+              values_from = empty_row, names_prefix = "temp_")
+
+
+# set up range of atmospheric d13C
+d13C_atm_range <- seq(sol_dat$d13C_atm_min[x], sol_dat$d13C_atm_max[x], 1)
 
 
 # calculate pcO2 value for each soil temp/ Sz combination
-for (i in seq_along(d13Cs)) {
-  for (j in seq_along(pCO2$Sz)) {
-    pCO2[i, j +1] <- pCO2$Sz[j] * ((d13Cs[i] - 1.0044 * sol_dat$d13Cresp[x] - 4.4) / (sol_dat$d13Catm[x] - d13Cs[i]))
+get_pCO2 <- function(d13C_atm_value) {
+  for (i in seq_along(d13Cs)) {
+    for (j in seq_along(pCO2$Sz)) {
+      pCO2[i, j +1] <-
+        pCO2$Sz[j] * ((d13Cs[i] - 1.0044 * sol_dat$d13Cresp[x] - 4.4) / 
+                        (d13C_atm_value - d13Cs[i]))
+    }
   }
+  
+  # convert to long format for proceeding
+  pCO2 %>%
+    pivot_longer(cols = -Sz, 
+                 names_to = "soil_temp",
+                 values_to = "pCO2")
 }
 
-# convert to long format for proceeding
-pCO2_long <- pCO2 %>% 
-  pivot_longer(cols = -Sz, names_to = "soil_temp", values_to = "pCO2")
+pCO2_long <- d13C_atm_range %>% 
+  map_dfr(get_pCO2)
+
+
+
 
 # bootstrapping mean
 # set seed
 set.seed(1234)
-boot_mean <- boot::boot(data = pCO2_long$pCO2,
+boot_mean <- boot(data = pCO2_long$pCO2,
                           statistic = mean.fun, R = 1000, sim="ordinary")
 
-boot_ci_mean <- boot::boot.ci(boot_mean, type = "bca")
+boot_ci_mean <- boot.ci(boot_mean, type = "bca")
 
 # save in data frame
 boot_result_mean <- c(as.double(boot_ci_mean$bca[4]),
@@ -81,10 +112,10 @@ boot_mean_df[boot_mean_df$sample_name == sol_dat$Sample[x],2:4] <- boot_result_m
 # same for median
 # set seed
 set.seed(1234)
-boot_median <- boot::boot(data = pCO2_long$pCO2,
+boot_median <- boot(data = pCO2_long$pCO2,
                         statistic = median.fun, R = 1000, sim="ordinary")
 
-boot_ci_median <- boot::boot.ci(boot_median, type = "bca")
+boot_ci_median <- boot.ci(boot_median, type = "bca")
 
 # save in data frame
 boot_result_median <- c(as.double(boot_ci_median$bca[4]),
