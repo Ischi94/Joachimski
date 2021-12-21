1ee1954b1503aeabb359ad435036ebab1cf05cbc
Ischi94 <gmathes94@gmx.de>
2021-12-21 08:57:24 +0100

    calculate pCO2 over wider range of Sz values

--- pCO2_code_ci.R
+++ pCO2_code_ci.R
@@ -3,6 +3,10 @@ library(tidyverse) # for data processing
 library(openxlsx) # for writing to excel file
 library(tidybayes) # for credible intervals
 
+
+# Data preparation --------------------------------------------------------
+
+
 # load data
 sol_dat <- readxl::read_excel("paleosol_data.xlsx")
 
@@ -18,7 +22,14 @@ sol_dat <- sol_dat %>%
          "S_z_min" = "S(z)", 
          "S_z_max" = "...12") %>% 
   # remove empty rows
-  drop_na(Sample)
+  drop_na(Sample) %>% 
+  # increase Sz_max to 2000
+  mutate(S_z_max = 2000)
+
+
+
+# pCO2 calculation --------------------------------------------------------
+
 
 
 # calculate distributions of possible 
@@ -36,7 +47,8 @@ sol_dat_dist <- sol_dat %>%
     S_z_range = map2(S_z_min, S_z_max, seq, by = 100)) %>% 
   select(Sample, S_z_range, d13Cs, d13Cresp, d13Ca_range) %>% 
   # expand to grid
-  unnest(c(S_z_range, d13Cs)) %>% 
+  unnest(S_z_range) %>%
+  unnest(d13Cs) %>% 
   unnest(d13Ca_range) %>% 
   group_split(Sample) %>% 
   map_df(expand, Sample, 
@@ -47,28 +59,105 @@ sol_dat_dist <- sol_dat %>%
                                (d13Ca_range - d13Cs))) %>% 
   group_by(Sample) 
   
-# calculate credible intervals 
 
+# with fixed S_Z_values
+sol_dat_fix <- sol_dat %>% 
+  mutate(
+    # set up temperature range
+    temp_range = map2(soil_temp_min, soil_temp_max, seq, by = 1), 
+    # calculate d13Cs
+    d13Cs = map2(d13Ccarb, temp_range, 
+                 ~ .x - (11.98 - 0.12 * .y)),
+    # set up atmospheric d13C range
+    d13Ca_range = map2(d13C_atm_min, d13C_atm_max, seq, by = 0.1), 
+    Sz_500 = 500, 
+    Sz_1000 = 1000, 
+    Sz_1500 = 1500) %>% 
+  # expand to grid
+  unnest(d13Cs) %>% 
+  unnest(d13Ca_range) %>% 
+  group_split(Sample) %>% 
+  map_df(expand, Sample,
+         Sz_500, 
+         Sz_1000, 
+         Sz_1500,
+         d13Cs, 
+         d13Cresp, d13Ca_range) %>% 
+  # calculate pCO2
+  mutate(pCO2_Sz_500 = Sz_500 * ((d13Cs - 1.0044 * d13Cresp - 4.4) / 
+                               (d13Ca_range - d13Cs)), 
+         pCO2_Sz_1000 = Sz_1000 * ((d13Cs - 1.0044 * d13Cresp - 4.4) / 
+                                (d13Ca_range - d13Cs)),
+         pCO2_Sz_1500 = Sz_1500 * ((d13Cs - 1.0044 * d13Cresp - 4.4) / 
+                                (d13Ca_range - d13Cs))) %>% 
+  group_by(Sample) 
+
+
+
+# Credible intervals ------------------------------------------------------
+
+
+## highest density intervals
 # mean
-sol_dat_mean <- sol_dat_dist %>% 
-  summarise(lwr_ci = mean_hdi(pCO2)[[2]], 
-            mean_pCO2 = mean_hdi(pCO2)[[1]], 
-            upr_ci = mean_hdi(pCO2)[[3]]) 
+sol_dat_mean_hdi <- sol_dat_dist %>% 
+  mean_hdi(pCO2) %>% 
+  select(Sample, lwr_ci = .lower,
+         mean_pCO2 = pCO2, 
+         upr_ci = .upper)
+
+
 
 # median
-sol_dat_median <- sol_dat_dist %>% 
-  summarise(lwr_ci = median_hdi(pCO2)[[2]], 
-            median_pCO2 = median_hdi(pCO2)[[1]], 
-            upr_ci = median_hdi(pCO2)[[3]]) 
-  
+sol_dat_median_hdi <- sol_dat_dist %>% 
+  median_hdi(pCO2) %>% 
+  select(Sample, lwr_ci = .lower,
+         mean_pCO2 = pCO2, 
+         upr_ci = .upper)
 
-# save to excel file
 
+## quantile intervals
 # mean
-write.xlsx(sol_dat_mean, file = "mean-pCO2.xlsx")
+sol_dat_mean_qi <- sol_dat_dist %>% 
+  mean_qi(pCO2) %>% 
+  select(Sample, lwr_ci = .lower,
+         mean_pCO2 = pCO2, 
+         upr_ci = .upper)
+
+
 
 # median
-write.xlsx(sol_dat_median, file = "median-pCO2.xlsx")
+sol_dat_median_qi <- sol_dat_dist %>% 
+  median_qi(pCO2) %>% 
+  select(Sample, lwr_ci = .lower,
+         mean_pCO2 = pCO2, 
+         upr_ci = .upper)
 
 
+### same for fixed Sz values
+# mean
+sol_dat_fix_mean_hdi <- sol_dat_fix %>% 
+  mean_hdi(pCO2_Sz_500, 
+            pCO2_Sz_1000, 
+            pCO2_Sz_1500) %>% 
+  select(-starts_with("."))
+
+
+
+# Save to excel file ------------------------------------------------------
+
+
+# mean hdi
+write.xlsx(sol_dat_mean_hdi, file = "mean-pCO2-hdi.xlsx")
+
+# median hdi
+write.xlsx(sol_dat_median_hdi, file = "median-pCO2-hdi.xlsx")
+
+# mean qi
+write.xlsx(sol_dat_mean_qi, file = "mean-pCO2-qi.xlsx")
+
+# median qi
+write.xlsx(sol_dat_median_qi, file = "median-pCO2-qi.xlsx")
+
+# fixed Sz_values
+write.xlsx(sol_dat_fix_mean_hdi, file = "mean-pCO2-hdi-fixed-Sz.xlsx")
 
